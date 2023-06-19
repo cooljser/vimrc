97d567bb3c3689fa95086481a27177f318cd1ec5
fan.hu <fan.hu3@envision-digital.com>
2023-06-19 16:29:09 +0800

    remove test config

--- lua-coc/lua/plugin-config/matchup.lua
+++
@@ -1,9 +0,0 @@
-return require('packer').startup(function(use)
-  use {
-    'andymass/vim-matchup',
-    setup = function()
-      -- may set any options here
-      vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    end
-  }
-end)
--- lua/lua/colorscheme.lua
+++
@@ -1,9 +0,0 @@
--- Lua
-require('onedark').setup {
-    style = 'darker'
-}
-
-require('onedark').load()
-
-vim.g.onedark_termcolors = 16
-vim.g.onedark_terminal_italics = 0
